be4d9807b78195ed95b5352c89f4bf84a8389ec9
Jeremi <Jeremilafarge@protonmail.com>
2024-05-03 01:32:00 +0200

    code final

--- src/main.c
+++ src/main.c
@@ -1,39 +1,41 @@
-#include <stdio.h> // Inclut la bibliothèque standard d'entrée/sortie pour utiliser des fonctions comme printf et scanf
-#include <string.h> // Inclut la bibliothèque de manipulation de chaînes pour utiliser des fonctions comme strcmp et strlen
-#include <stdbool.h> // Inclut la bibliothèque standard bool pour utiliser le type de données booléen
+#include <stdio.h>
+#include <string.h> //strcmp compare deux chaines de carac et strlen donne la longueur d'une chaine de carac
+#include <stdbool.h>  //Permet d'utiliser la fonction booléenne
+#include <unistd.h>
 
-char password[] = "azerty12"; // Déclare une variable globale de type tableau de caractères pour stocker le mot de passe
+char password[] = "azerty12";
 
 // Déclare une fonction qui compare le mot de passe entré par l'utilisateur avec le mot de passe stocké
 bool compare(char user_pass[]){
     if (strcmp(user_pass, password) == 0){ // Si le mot de passe entré par l'utilisateur est le même que le mot de passe stocké
-        printf("Bravo, tu as trouve le mot de passe\n"); // Affiche un message de réussite
-        return true; // Retourne vrai
-    } else { // Sinon
-        printf("Cherche encore\n"); // Affiche un message d'échec
-        return false; // Retourne faux
+        printf("Bravo, tu as trouve le mot de passe\n");
+        return true;
+    } else {
+        printf("Cherche encore\n");
+        return false;
     }
 }
 
 // Déclare une fonction qui trouve et affiche les caractères communs dans le bon ordre entre le mot de passe entré par l'utilisateur et le mot de passe stocké
 void element_commun(char user_pass[]){
     int i; // Déclare une variable de type entier pour la boucle
-    printf("Les caracteres communs dans le bon ordre sont : "); // Affiche un message
+    printf("Les caracteres communs dans le bon ordre sont : ");
     for(i = 0; i < strlen(password); i++){ // Parcourt chaque caractère du mot de passe stocké
-        printf("%c ", (i < strlen(user_pass) && password[i] == user_pass[i]) ? password[i] : '_'); // Affiche le caractère commun si il y en a un, sinon affiche un X
+        printf("%c ", (i < strlen(user_pass) && password[i] == user_pass[i]) ? password[i] : '_'); // Affiche le caractère commun si il y en a un, sinon affiche un _
     }
-    printf("\n"); // Passe à la ligne suivante
+    printf("\n");
 }
 
 // Déclare la fonction principale du programme
 int main(){
     char user_pass[50]; // Déclare une variable de type tableau de caractères pour stocker le mot de passe entré par l'utilisateur
-    bool guessed = false; // Déclare une variable de type booléen pour savoir si le mot de passe a été deviné
-    while(!guessed){ // Tant que le mot de passe n'a pas été deviné
-        printf("devine le mot de passe : \n"); // Demande à l'utilisateur de deviner le mot de passe
+    bool guessed = false;
+    while(!guessed){
+        printf("devine le mot de passe : \n");
         scanf("%s", user_pass); // Lit le mot de passe entré par l'utilisateur
-        element_commun(user_pass); // Appelle la fonction pour trouver et afficher les caractères communs dans le bon ordre
-        guessed = compare(user_pass); // Appelle la fonction pour comparer le mot de passe entré par l'utilisateur avec le mot de passe stocké et stocke le résultat
+        element_commun(user_pass);
+        guessed = compare(user_pass);
     }
-    return 0; // Termine le programme avec le code de sortie 0
+    sleep(5);
+    return 0;
 }
